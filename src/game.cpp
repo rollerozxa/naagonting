0b38f63295b3a02b282144330e95d291796997fd
ROllerozxa <temporaryemail4meh+github@gmail.com>
2022-01-06 22:04:43 +0100

    No dynamic rect size. :unamused:

--- src/game.cpp
+++ src/game.cpp
@@ -8,12 +8,10 @@ void game_update(SDL_Event e) {
 	SDL_GetMouseState(&mX, &mY);
 	playerRect.x = mX - 50;
 	playerRect.y = mY - 50;
-
-	playerRect.w = mX;
-	playerRect.h = mY;
 }
 
 void game_draw(SDL_Renderer *renderer) {
 	SDL_SetRenderDrawColor(renderer, 0xAA, 0x22, 0x22, 0xFF);
 	SDL_RenderFillRect(renderer, &playerRect);
 }
+
